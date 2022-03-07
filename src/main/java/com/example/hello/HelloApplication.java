079a38eec767f6548b9de0161bae34e7e66b97cc
portuna85 <portuna85@gmail.com>
2022-03-07 17:36:14 +0900

    Initial commit

---
+++ src/main/java/com/example/hello/HelloApplication.java
@@ -0,0 +1,16 @@
+package com.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+
+@SpringBootApplication
+@ServletComponentScan
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+
+}
