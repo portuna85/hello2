5e769c8063f3c4aa2c8d012a671315bedf5df124
portuna85 <portuna85@gmail.com>
2022-03-08 06:07:58 +0900

    Spring MVC BASIC

--- src/main/java/com/example/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
+++ src/main/java/com/example/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -1,8 +1,8 @@
 package com.example.hello.servlet.web.frontcontroller.v3;
 
 import com.example.hello.servlet.web.frontcontroller.ModelView;
-
 import com.example.hello.servlet.web.frontcontroller.v1.MyView;
+
 import com.example.hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import com.example.hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import com.example.hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
@@ -16,44 +16,44 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV3", urlPatterns = "/frontcontroller/v3/*")
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
 public class FrontControllerServletV3 extends HttpServlet {
     private Map<String, ControllerV3> controllerMap = new HashMap<>();
 
     public FrontControllerServletV3() {
-        controllerMap.put("/front-controller/v3/members/new-form", new
-                MemberFormControllerV3());
-        controllerMap.put("/front-controller/v3/members/save", new
-                MemberSaveControllerV3());
-        controllerMap.put("/front-controller/v3/members", new
-                MemberListControllerV3());
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
     }
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
         ControllerV3 controller = controllerMap.get(requestURI);
         if (controller == null) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
+
+        // paramMap
+
         Map<String, String> paramMap = createParamMap(request);
         ModelView mv = controller.process(paramMap);
+
         String viewName = mv.getViewName();
         MyView view = viewResolver(viewName);
+
         view.render(mv.getModel(), request, response);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 
     private Map<String, String> createParamMap(HttpServletRequest request) {
         Map<String, String> paramMap = new HashMap<>();
         request.getParameterNames().asIterator()
-               .forEachRemaining(paramName -> paramMap.put(paramName,
-                       request.getParameter(paramName)));
+               .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
         return paramMap;
-    }
-
-    private MyView viewResolver(String viewName) {
-        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 }
