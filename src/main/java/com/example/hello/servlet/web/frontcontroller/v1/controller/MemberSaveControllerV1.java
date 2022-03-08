9aea10b878cc90b8daefb858298cf2901088d139
portuna85 <portuna85@gmail.com>
2022-03-08 11:23:42 +0900

    Spring MVC BASIC

--- src/main/java/com/example/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
+++ src/main/java/com/example/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -9,7 +9,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Properties;
 
 public class MemberSaveControllerV1 implements ControllerV1 {
 
