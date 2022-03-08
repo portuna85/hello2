e264fd1cf3f0366b86fa1ba6b7c28e754a988824
portuna85 <portuna85@gmail.com>
2022-03-08 13:11:31 +0900

    Spring MVC BASIC

--- src/main/java/com/example/hello/servlet/web/frontcontroller/v1/MyView.java
+++ src/main/java/com/example/hello/servlet/web/frontcontroller/v1/MyView.java
@@ -14,21 +14,18 @@ public class MyView {
         this.viewPath = viewPath;
     }
 
-    public void render(HttpServletRequest request, HttpServletResponse
-            response) throws ServletException, IOException {
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
 
-    public void render(Map<String, Object> model, HttpServletRequest request,
-                       HttpServletResponse response) throws ServletException, IOException {
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         modelToRequestAttribute(model, request);
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
 
-    private void modelToRequestAttribute(Map<String, Object> model,
-                                         HttpServletRequest request) {
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
         model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
