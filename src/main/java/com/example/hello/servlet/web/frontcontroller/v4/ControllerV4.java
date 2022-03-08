1305d05fad61ca7bbed6bf4adde38d7bb07bc033
portuna85 <portuna85@gmail.com>
2022-03-08 17:14:57 +0900

    Spring MVC BASIC

---
+++ src/main/java/com/example/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,12 @@
+package com.example.hello.servlet.web.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+    /*
+    @param paramMap
+    @param Model
+    @return viewName
+     */
+    String process(Map<String, String> paramMap, Map<String, Object> model);
+}
