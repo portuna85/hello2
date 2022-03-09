6892e4646316cf99d3e54c5a6be720cedef23118
portuna85 <portuna85@gmail.com>
2022-03-09 15:34:28 +0900

    Spring MVC BASIC

--- src/main/java/com/example/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/com/example/hello/servlet/basic/request/RequestParamServlet.java
@@ -7,13 +7,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * 1. 파라미터 전송 기능
- * http://localhost:8080/request-param?username=hello&age=20
- * <p>
- * 2. 동일한 파라미터 전송 가능
- * http://localhost:8080/request-param?username=hello&username=kim&age=20
- */
+        /**
+         * 1. 파라미터 전송 기능
+         * http://localhost:8080/request-param?username=hello&age=20
+         * <p>
+         * 2. 동일한 파라미터 전송 가능
+         * http://localhost:8080/request-param?username=hello&username=kim&age=20
+         */
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
     @Override
@@ -31,12 +31,14 @@ public class RequestParamServlet extends HttpServlet {
                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
         System.out.println("[전체 파라미터 조회] - end");
         System.out.println();
+
         System.out.println("[단일 파라미터 조회]");
         String username = request.getParameter("username");
         System.out.println("request.getParameter(username) = " + username);
         String age = request.getParameter("age");
         System.out.println("request.getParameter(age) = " + age);
         System.out.println();
+
         System.out.println("[이름이 같은 복수 파라미터 조회]");
         System.out.println("request.getParameterValues(username)");
         String[] usernames = request.getParameterValues("username");
